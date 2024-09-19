aba46a91f1622bdf3f0b27098a855844552f524e
Koen Bloemen <koen.bloemen@pxl.be>
2024-09-19 09:22:28 +0200

    final

---
+++ Methods-3---Rekenmachine2-herwerkt-naar-methods/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Methods_3___Rekenmachine2_herwerkt_naar_methods
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Methods-3---Rekenmachine2-herwerkt-naar-methods/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Methods_3___Rekenmachine2_herwerkt_naar_methods
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private float FirstNumber = 0.0f;
+        private float SecondNumber = 0.0f;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadNumbers(string firstNumberText, string secondNumberText)
+        {
+            FirstNumber = float.Parse(firstNumberText);
+            SecondNumber = float.Parse(secondNumberText);
+        }
+
+        // char is een karakter (letter, symbool, cijfer,...)
+        private float Calculate(char sign)
+        {
+            switch (sign)
+            {
+                case '+':
+                    return FirstNumber + SecondNumber;
+                case '-':
+                    return FirstNumber - SecondNumber;
+                case '*':
+                    return FirstNumber * SecondNumber;
+                case '/':
+                    return FirstNumber / SecondNumber;
+                default:
+                    // Functie method moet altijd een resultaat returnen!
+                    return 0.0f;
+            }
+        }
+
+
+        private void plusButton_Click(object sender, RoutedEventArgs e)
+        {
+            ReadNumbers(firstNumberTextBox.Text, secondNumberTextBox.Text);
+            float resultaat = Calculate('+');
+            resultTextBox.Text = resultaat.ToString();
+            firstNumberTextBox.Focus();
+        }
+
+        private void minusButton_Click(object sender, RoutedEventArgs e)
+        {
+            ReadNumbers(firstNumberTextBox.Text, secondNumberTextBox.Text);
+            float resultaat = Calculate('-');
+            resultTextBox.Text = resultaat.ToString();
+            firstNumberTextBox.Focus();
+        }
+
+        private void multiplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            ReadNumbers(firstNumberTextBox.Text, secondNumberTextBox.Text);
+            float resultaat = Calculate('*');
+            resultTextBox.Text = resultaat.ToString();
+            firstNumberTextBox.Focus();
+        }
+
+        private void divideButton_Click(object sender, RoutedEventArgs e)
+        {
+            ReadNumbers(firstNumberTextBox.Text, secondNumberTextBox.Text);
+            float resultaat = Calculate('/');
+            resultTextBox.Text = resultaat.ToString();
+            firstNumberTextBox.Focus();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumberTextBox.Text = "0";
+            secondNumberTextBox.Text = "0";
+            resultTextBox.Clear();
+            firstNumberTextBox.Focus();
+        }
+    }
+}
